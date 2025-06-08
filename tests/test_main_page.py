a63ce7b8db7ae3bd2579cb1be452af67a37b9df9
ifyod <ifyodrv@yandex.ru>
2025-06-08 23:55:17 +0300

    First test using PageObject

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pages.main_page import MainPage
+
 
 
 def go_to_login_page(browser):
@@ -10,5 +12,7 @@ def go_to_login_page(browser):
     login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
-   browser.get(link)
-   go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
