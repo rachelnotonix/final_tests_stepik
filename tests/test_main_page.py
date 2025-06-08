940c297b0b98eca1ea6df52914e4cdb542359046
ifyod <ifyodrv@yandex.ru>
2025-06-08 23:02:13 +0300

    PageObject files to build up architechture

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -5,8 +5,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+   browser.get(link)
+   go_to_login_page(browser)
